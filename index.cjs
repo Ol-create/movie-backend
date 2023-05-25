9d6a0943fcad20d67a773af1e38a0e955b25d4ad
Ol-create <oluola96@gmail.com>
2023-05-25 10:06:56 +0100

    add validation to post endpoint

--- index.cjs
+++ index.cjs
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-
+app.use(express.json());
 
 
 const courses = [
-    { id: 1, course_name: 'Software Dev' },
-    { id: 2, course_name: 'Mathematics' },
-    {id: 3, course_name: 'English Language'}
+    { "id": 1, "course_name": 'Software Dev' },
+    { "id": 2, "course_name": 'Mathematics' },
+    {"id": 3, "course_name": 'English Language'}
 ]
 const port = process.env.PORT || 3000
 
@@ -23,4 +23,22 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(course)
 })
 
+// Post course to 'api/courses/' end_point
+app.post('/api/courses/', (req, res) => {
+    if (!req.body.course_name || req.body.course_name.length < 3) {
+        res.status(400).send('Enter the course or the minimum character allowed is 3')
+        return;
+    }
+    let course = {
+        id: courses.length + 1,
+        course_name: req.body.course_name,
+    }
+    courses.push(course);
+    res.send(course).status(201)
+})
+
+// Get all courses
+app.get('api/all/courses', (req, res) => {
+    res.send(courses)
+})
 app.listen(port, () => console.log(`Listening on port ${port}`))
