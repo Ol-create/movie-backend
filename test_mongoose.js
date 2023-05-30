40fcb54f1e4d7e0cebdd8b190ea59f54208b80cc
Ol-create <oluola96@gmail.com>
2023-05-30 02:43:58 +0100

    creat course schema

--- test_mongoose.js
+++ test_mongoose.js
@@ -4,3 +4,10 @@ mongoose.connect('mongodb://localhost/mycourses')
     .then(() => console.log('Connction to MongoDB was successsful...'))
     .catch(() => console.error('Error occured, could not connect to MongoDB'))
 
+const courseSchema = new mongoose.Schema({
+    course_name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublish: Boolean,
+});
