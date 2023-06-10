bbef14c09300326bd499d4e84b46c113cdfa8c0b
Ol-create <oluola96@gmail.com>
2023-06-10 21:47:25 +0100

    add mongoose exercise solution

--- solution.js
+++ solution.js
@@ -1,12 +1,26 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/myExercise', () => console.log("Conneted to myExercise database successfully"))
+mongoose.connect('mongodb://localhost/myExercise')
+    .then(() => console.log("Conneted to myExercise database successfully"))
 
-const exerciseScchma = mongoose.Schema({
+
+const exerciseScchma = new mongoose.Schema({
     tags: [String],
     date: { type: Date, default: Date.now() },
     name: String,
     author: String,
     price: Number,
-    isPublish: Boolean,
+    isPublished: Boolean,
 })
+
+const Courses = mongoose.model('subjects', exerciseScchma);
+
+const solution = async function () {
+    const courses = await Courses.find({ tags: "backend", isPublished: true })
+        .select({ name: 1, author: 1 })
+        .sort({ name: 1 })
+     
+    console.log(courses);
+}
+
+solution();
